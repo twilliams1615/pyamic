44e576eb3000e1cfe964adb02e192678ee0a6352
Mike <mikerah@gmail.com>
2018-11-21 13:52:53 -0600

    skeleton

--- pyamic.py
+++ pyamic.py
@@ -1,3 +1,38 @@
 """
 pyamic
 """
+
+import argparse
+import oyaml as yaml
+from jinja2 import Environment, FileSystemLoader
+
+
+def laoad_template(source_template):
+    """
+    """
+    file_loader = FileSystemLoader('templates')
+    env = Environment(loader=file_loader)
+    template = env.get_template(source_template)
+    return(template.render())
+
+
+def read_source(source):
+    """
+    """
+    with open(source, "r") as data:
+        return(yaml.load(source))
+
+
+if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser
+    PARSER.add_argument("source", help="source files", required= True)
+    ARGS = PARSER.parse_args()
+
+    data = read_source(ARGS.source)
+    html = load_template(data["template"])
+
+
+
+    
+
+
