f075761698700f00cb0851cdefe59b94c5010bf2
Mike <mikerah@gmail.com>
2018-11-21 14:39:01 -0600

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pyamic
-Generate Static Sites from Yaml source with Python 
+Generate Static Sites from Yaml source with Python
+
+Pyamic is a Static Site Genrator. It uses YAML files for its configuration and for its source. It uses the Jinja2 templating library. My goal is to create a super simple static site generator.
--- pyamic.py
+++ pyamic.py
@@ -1,13 +1,14 @@
 """
 pyamic
 """
-
+from os import listdir
+from os.path import isfile, jooin
 import argparse
 import oyaml as yaml
 from jinja2 import Environment, FileSystemLoader
 
 
-def laoad_template(source_template):
+def load_template(source_template):
     """
     """
     file_loader = FileSystemLoader('templates')
@@ -23,13 +24,23 @@ def read_source(source):
         return(yaml.load(source))
 
 
+def read_directory(mypath):
+    """
+    """
+    return([f for f in listdir(mypath) if isfile(mypath, f)])
+
+
+
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser
-    PARSER.add_argument("source", help="source files", required= True)
+    PARSER.add_argument("config", help="config file", required= True)
     ARGS = PARSER.parse_args()
 
+    
+
+    posts = read_directory(ARGS.posts)
     data = read_source(ARGS.source)
-    html = load_template(data["template"])
+    html = load_template(data["templates"])
 
 
 
